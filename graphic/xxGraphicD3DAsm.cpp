74452ee3c8dc902c26b0a8026772530b49d433e8
TAiGA <@>
2019-08-23 17:42:27 +0800

    Add Direct3D 10 Assembly Shader

--- graphic/xxGraphicD3DAsm.cpp
+++ graphic/xxGraphicD3DAsm.cpp
@@ -1,27 +1,28 @@
 #include "xxGraphicD3DAsm.h"
 
 #include "dxsdk/d3d9.h"
+#include "dxsdk/d3d10TokenizedProgramFormat.hpp"
 
 //==============================================================================
-//  Direct3D Assembly Shader
+//  Direct3D 8.0 Assembly Shader
 //==============================================================================
-#define D3DSP_WRITEMASK_X           0x00010000
-#define D3DSP_WRITEMASK_XY          0x00030000
-#define D3DSP_WRITEMASK_XYZ         0x00070000
-#define D3DSP_WRITEMASK_XYZW        0x000F0000
-#define D3DDECLUSAGE_UNKNOWN        D3DSTT_UNKNOWN
-#define D3DDECLUSAGE_2D             D3DSTT_2D
-#define D3DDECLUSAGE_CUBE           D3DSTT_CUBE
-#define D3DDECLUSAGE_VOLUME         D3DSTT_VOLUME
-#define _SIO(sio, length)           D3DSIO_ ## sio | ((length << D3DSI_INSTLENGTH_SHIFT) & D3DSI_INSTLENGTH_MASK)
-#define _TOKEN(mask, value)         ((value << D3DSP_ ## mask ## _SHIFT))                             | 0x80000000
-#define _TOKEN1(mask, value)        ((value << D3DSP_ ## mask ## _SHIFT)  & D3DSP_ ## mask ## _MASK)  | 0x80000000
-#define _TOKEN2(mask, value)        ((value << D3DSP_ ## mask ## _SHIFT2) & D3DSP_ ## mask ## _MASK2) | _TOKEN1(mask, value)
-#define _DCL(dcl)                   _TOKEN(DCL_USAGE, D3DDECLUSAGE_ ## dcl)
-#define _SRC(reg, index)            _TOKEN2(REGTYPE, D3DSPR_ ## reg) | D3DVS_W_W     | D3DVS_Z_Z     | D3DVS_Y_Y     | D3DVS_X_X     | index
-#define _SRCM(reg, index, m)        _TOKEN2(REGTYPE, D3DSPR_ ## reg) | D3DVS_W_ ## m | D3DVS_Z_ ## m | D3DVS_Y_ ## m | D3DVS_X_ ## m | index
-#define _DST(reg, index)            _TOKEN2(REGTYPE, D3DSPR_ ## reg) | D3DSP_WRITEMASK_ALL   | index
-#define _DSTM(reg, index, m)        _TOKEN2(REGTYPE, D3DSPR_ ## reg) | D3DSP_WRITEMASK_ ## m | index
+#define D3DSP_WRITEMASK_X       0x00010000
+#define D3DSP_WRITEMASK_XY      0x00030000
+#define D3DSP_WRITEMASK_XYZ     0x00070000
+#define D3DSP_WRITEMASK_XYZW    0x000F0000
+#define D3DDECLUSAGE_UNKNOWN    D3DSTT_UNKNOWN
+#define D3DDECLUSAGE_2D         D3DSTT_2D
+#define D3DDECLUSAGE_CUBE       D3DSTT_CUBE
+#define D3DDECLUSAGE_VOLUME     D3DSTT_VOLUME
+#define _SIO(sio, length)       (D3DSIO_ ## sio | (length << D3DSI_INSTLENGTH_SHIFT))
+#define _TOKEN(mask, value)     ((value << D3DSP_ ## mask ## _SHIFT))                             | 0x80000000
+#define _TOKEN1(mask, value)    ((value << D3DSP_ ## mask ## _SHIFT)  & D3DSP_ ## mask ## _MASK)  | 0x80000000
+#define _TOKEN2(mask, value)    ((value << D3DSP_ ## mask ## _SHIFT2) & D3DSP_ ## mask ## _MASK2) | _TOKEN1(mask, value)
+#define _DCL(dcl)               _TOKEN(DCL_USAGE, D3DDECLUSAGE_ ## dcl)
+#define _SRC(reg, index)        _TOKEN2(REGTYPE, D3DSPR_ ## reg) | D3DVS_W_W     | D3DVS_Z_Z     | D3DVS_Y_Y     | D3DVS_X_X     | index
+#define _SRCM(reg, index, m)    _TOKEN2(REGTYPE, D3DSPR_ ## reg) | D3DVS_W_ ## m | D3DVS_Z_ ## m | D3DVS_Y_ ## m | D3DVS_X_ ## m | index
+#define _DST(reg, index)        _TOKEN2(REGTYPE, D3DSPR_ ## reg) | D3DSP_WRITEMASK_ALL   | index
+#define _DSTM(reg, index, m)    _TOKEN2(REGTYPE, D3DSPR_ ## reg) | D3DSP_WRITEMASK_ ## m | index
 //------------------------------------------------------------------------------
 const DWORD vertexShaderCode10[] =
 { 
@@ -70,110 +71,105 @@ const DWORD pixelShaderCode20[] =
     _SIO(END, 0)
 };
 //------------------------------------------------------------------------------
+#undef _SRC
+#undef _SRCM
+#undef _DST
+#undef _DSTM
+//==============================================================================
+//  Direct3D 10.0 Assembly Shader
+//==============================================================================
+#define _DXBC                                   0x43425844
+#define _ISGN                                   0x4e475349
+#define _OSGN                                   0x4e47534f
+#define _SHDR                                   0x52444853
+#define D3D10_SB_4_COMPONENT_XXXX               0x00
+#define D3D10_SB_4_COMPONENT_XYXX               0x04
+#define D3D10_SB_4_COMPONENT_XYZW               0xe4
+#define D3D10_SB_4_COMPONENT_YYYY               0x55
+#define D3D10_SB_4_COMPONENT_ZZZZ               0xaa
+#define D3D10_SB_4_COMPONENT_WWWW               0xff
+#define D3D10_SB_OPERAND_4_COMPONENT_MASK_XY    0x30
+#define D3D10_SB_OPERAND_4_COMPONENT_MASK_XYZ   0x70
+#define D3D10_SB_OPERAND_4_COMPONENT_MASK_XYZW  0xf0
+#define D3D10_SB_RETURN_TYPE_FLOAT4             (D3D10_SB_RETURN_TYPE_FLOAT << 0) | \
+                                                (D3D10_SB_RETURN_TYPE_FLOAT << 4) | \
+                                                (D3D10_SB_RETURN_TYPE_FLOAT << 8) | \
+                                                (D3D10_SB_RETURN_TYPE_FLOAT << 12)
+#define _VER(type, major, minor)                (ENCODE_D3D10_SB_TOKENIZED_PROGRAM_VERSION_TOKEN(D3D10_SB_ ## type, major, minor))
+#define _OP(opcode, length)                     (D3D10_SB_OPCODE_ ## opcode | ENCODE_D3D10_SB_TOKENIZED_INSTRUCTION_LENGTH(length))
+#define _OPDIM(opcode, length, dim)             (_OP(opcode, length) | ENCODE_D3D10_SB_RESOURCE_DIMENSION(D3D10_SB_RESOURCE_DIMENSION_ ## dim))
+#define _OPINT(opcode, length, int)             (_OP(opcode, length) | ENCODE_D3D10_SB_INPUT_INTERPOLATION_MODE(D3D10_SB_INTERPOLATION_ ## int))
+#define _0(index, reg)                          ENCODE_D3D10_SB_OPERAND_INDEX_DIMENSION(D3D10_SB_OPERAND_INDEX_ ## index) | \
+                                                ENCODE_D3D10_SB_OPERAND_TYPE(D3D10_SB_OPERAND_TYPE_ ## reg) | \
+                                                ENCODE_D3D10_SB_OPERAND_NUM_COMPONENTS(D3D10_SB_OPERAND_0_COMPONENT)
+#define _4MM(index, reg, mask)                  ENCODE_D3D10_SB_OPERAND_INDEX_DIMENSION(D3D10_SB_OPERAND_INDEX_ ## index) | \
+                                                ENCODE_D3D10_SB_OPERAND_TYPE(D3D10_SB_OPERAND_TYPE_ ## reg) | \
+                                                D3D10_SB_OPERAND_4_COMPONENT_MASK_ ## mask | \
+                                                ENCODE_D3D10_SB_OPERAND_4_COMPONENT_SELECTION_MODE(D3D10_SB_OPERAND_4_COMPONENT_MASK_MODE) | \
+                                                ENCODE_D3D10_SB_OPERAND_NUM_COMPONENTS(D3D10_SB_OPERAND_4_COMPONENT)
+#define _4M(index, reg)                         _4MM(index, reg, XYZW)
+#define _4SS(index, reg, swizzle)               ENCODE_D3D10_SB_OPERAND_INDEX_DIMENSION(D3D10_SB_OPERAND_INDEX_ ## index) | \
+                                                ENCODE_D3D10_SB_OPERAND_TYPE(D3D10_SB_OPERAND_TYPE_ ## reg) | \
+                                                (D3D10_SB_4_COMPONENT_ ## swizzle << D3D10_SB_OPERAND_4_COMPONENT_SWIZZLE_SHIFT) | \
+                                                ENCODE_D3D10_SB_OPERAND_4_COMPONENT_SELECTION_MODE(D3D10_SB_OPERAND_4_COMPONENT_SWIZZLE_MODE) | \
+                                                ENCODE_D3D10_SB_OPERAND_NUM_COMPONENTS(D3D10_SB_OPERAND_4_COMPONENT)
+#define _4S(index, reg)                         _4SS(index, reg, XYZW)
+//------------------------------------------------------------------------------
 const DWORD vertexShaderCode40[] =
 {
-    0x43425844, 0x1b72f980, 0xf6ba72d7, 0xb4c95127,
-    0x8b3cb55e, 0x00000001, 0x00000390, 0x00000005,
-    0x00000034, 0x00000100, 0x00000170, 0x000001e4,
-    0x00000314, 0x46454452, 0x000000c4, 0x00000001,
-    0x00000048, 0x00000001, 0x0000001c, 0xfffe0400,
-    0x00000100, 0x0000009c, 0x0000003c, 0x00000000,
-    0x00000000, 0x00000000, 0x00000000, 0x00000000,
-    0x00000001, 0x00000000, 0x6f6c4724, 0x736c6162,
-    0xababab00, 0x0000003c, 0x00000001, 0x00000060,
-    0x00000040, 0x00000000, 0x00000000, 0x00000078,
-    0x00000000, 0x00000040, 0x00000002, 0x0000008c,
-    0x00000000, 0x6a6f7250, 0x69746365, 0x614d6e6f,
-    0x78697274, 0xababab00, 0x00030003, 0x00040004,
-    0x00000000, 0x00000000, 0x7263694d, 0x666f736f,
-    0x52282074, 0x4c482029, 0x53204c53, 0x65646168,
-    0x6f432072, 0x6c69706d, 0x31207265, 0x00312e30,
-    0x4e475349, 0x00000068, 0x00000003, 0x00000008,
-    0x00000050, 0x00000000, 0x00000000, 0x00000003,
-    0x00000000, 0x00000707, 0x00000059, 0x00000000,
-    0x00000000, 0x00000003, 0x00000001, 0x00000f0f,
-    0x0000005f, 0x00000000, 0x00000000, 0x00000003,
-    0x00000002, 0x00000303, 0x49534f50, 0x4e4f4954,
-    0x4c4f4300, 0x5400524f, 0x4f435845, 0x0044524f,
-    0x4e47534f, 0x0000006c, 0x00000003, 0x00000008,
-    0x00000050, 0x00000000, 0x00000001, 0x00000003,
-    0x00000000, 0x0000000f, 0x0000005c, 0x00000000,
-    0x00000000, 0x00000003, 0x00000001, 0x0000000f,
-    0x00000062, 0x00000000, 0x00000000, 0x00000003,
-    0x00000002, 0x00000c03, 0x505f5653, 0x5449534f,
-    0x004e4f49, 0x4f4c4f43, 0x45540052, 0x4f4f4358,
-    0xab004452, 0x52444853, 0x00000128, 0x00010040,
-    0x0000004a, 0x04000059, 0x00208e46, 0x00000000,
-    0x00000004, 0x0300005f, 0x00101072, 0x00000000,
-    0x0300005f, 0x001010f2, 0x00000001, 0x0300005f,
-    0x00101032, 0x00000002, 0x04000067, 0x001020f2,
-    0x00000000, 0x00000001, 0x03000065, 0x001020f2,
-    0x00000001, 0x03000065, 0x00102032, 0x00000002,
-    0x02000068, 0x00000001, 0x08000038, 0x001000f2,
-    0x00000000, 0x00101556, 0x00000000, 0x00208e46,
-    0x00000000, 0x00000001, 0x0a000032, 0x001000f2,
-    0x00000000, 0x00208e46, 0x00000000, 0x00000000,
-    0x00101006, 0x00000000, 0x00100e46, 0x00000000,
-    0x0a000032, 0x001000f2, 0x00000000, 0x00208e46,
-    0x00000000, 0x00000002, 0x00101aa6, 0x00000000,
-    0x00100e46, 0x00000000, 0x08000000, 0x001020f2,
-    0x00000000, 0x00100e46, 0x00000000, 0x00208e46,
-    0x00000000, 0x00000003, 0x05000036, 0x001020f2,
-    0x00000001, 0x00101e46, 0x00000001, 0x05000036,
-    0x00102032, 0x00000002, 0x00101046, 0x00000002,
-    0x0100003e, 0x54415453, 0x00000074, 0x00000007,
-    0x00000001, 0x00000000, 0x00000006, 0x00000004,
-    0x00000000, 0x00000000, 0x00000001, 0x00000000,
-    0x00000000, 0x00000000, 0x00000000, 0x00000000,
-    0x00000000, 0x00000000, 0x00000000, 0x00000000,
-    0x00000000, 0x00000000, 0x00000002, 0x00000000,
-    0x00000000, 0x00000000, 0x00000000, 0x00000000,
-    0x00000000, 0x00000000, 0x00000000, 0x00000000,
+    _DXBC, 0xe51cbc0d, 0x8dacc42e, 0x6b679e5d, 0xc68b0d11,
+    0x00000001, 0x00000240, 3,
+    0x0000002c, 0x0000009c, 0x00000110,
+    _ISGN, 0x00000068, 3, 0x00000008,
+    0x00000050, 0, 0, 3, 0, 0x0707,
+    0x00000059, 0, 0, 3, 1, 0x0f0f,
+    0x0000005f, 0, 0, 3, 2, 0x0303,
+    0x49534f50, 0x4e4f4954, 0x4c4f4300, 0x5400524f, 0x4f435845, 0x0044524f,
+    _OSGN, 0x0000006c, 3, 0x00000008,
+    0x00000050, 0, 1, 3, 0, 0x000f,
+    0x0000005c, 0, 0, 3, 1, 0x000f,
+    0x00000062, 0, 0, 3, 2, 0x0c03,
+    0x505f5653, 0x5449534f, 0x004e4f49, 0x4f4c4f43, 0x45540052, 0x4f4f4358, 0xab004452,
+    _SHDR, 0x00000128, _VER(VERTEX_SHADER, 4, 0), 0x0000004a,
+    _OP(DCL_CONSTANT_BUFFER, 4), _4S(2D, CONSTANT_BUFFER), D3D10_SB_CONSTANT_BUFFER_IMMEDIATE_INDEXED, 4,       // dcl_constantbuffer CB0[4], immediateIndexed
+    _OP(DCL_INPUT, 3), _4MM(1D, INPUT, XYZ), 0,                                                                 // dcl_input v0.xyz
+    _OP(DCL_INPUT, 3), _4M(1D, INPUT), 1,                                                                       // dcl_input v1.xyzw
+    _OP(DCL_INPUT, 3), _4MM(1D, INPUT, XY), 2,                                                                  // dcl_input v2.xy
+    _OP(DCL_OUTPUT_SIV, 4), _4M(1D, OUTPUT), 0, 1,                                                              // dcl_output o0.xyzw
+    _OP(DCL_OUTPUT, 3), _4M(1D, OUTPUT), 1,                                                                     // dcl_output o1.xyzw
+    _OP(DCL_OUTPUT, 3), _4MM(1D, OUTPUT, XY), 2,                                                                // dcl_output o2.xy
+    _OP(DCL_TEMPS, 2), 1,                                                                                       // dcl_temps 1
+    _OP(MUL, 8), _4M(1D, TEMP), 0, _4SS(1D, INPUT, XXXX), 0, _4S(2D, CONSTANT_BUFFER), 0, 0,                    // mul r0.xyzw, v0.xxxx, cb0[0].xyzw
+    _OP(MAD, 10), _4M(1D, TEMP), 0, _4S(2D, CONSTANT_BUFFER), 0, 1, _4SS(1D, INPUT, YYYY), 0, _4S(1D, TEMP), 0, // mad r0.xyzw, cb0[1].xyzw, v0.yyyy, r0.xyzw
+    _OP(MAD, 10), _4M(1D, TEMP), 0, _4S(2D, CONSTANT_BUFFER), 0, 2, _4SS(1D, INPUT, ZZZZ), 0, _4S(1D, TEMP), 0, // mad r0.xyzw, cb0[2].xyzw, v0.zzzz, r0.xyzw
+    _OP(ADD, 8), _4M(1D, OUTPUT), 0, _4S(1D, TEMP), 0, _4S(2D, CONSTANT_BUFFER), 0, 3,                          // add o0.xyzw, r0.xyzw, cb0[3].xyzw
+    _OP(MOV, 5), _4M(1D, OUTPUT), 1, _4S(1D, INPUT), 1,                                                         // mov o1.xyzw, v1.xyzw
+    _OP(MOV, 5), _4MM(1D, OUTPUT, XY), 2, _4SS(1D, INPUT, XYXX), 2,                                             // mov o2.xy, v2.xyxx
+    _OP(RET, 1)                                                                                                 // ret
 };
 //------------------------------------------------------------------------------
 const DWORD pixelShaderCode40[] =
 {
-    0x43425844, 0x7285ce9e, 0xb45297a7, 0xfa28c46d,
-    0xc1d2c010, 0x00000001, 0x00000294, 0x00000005,
-    0x00000034, 0x000000d4, 0x00000148, 0x0000017c,
-    0x00000218, 0x46454452, 0x00000098, 0x00000000,
-    0x00000000, 0x00000002, 0x0000001c, 0xffff0400,
-    0x00000100, 0x0000006e, 0x0000005c, 0x00000003,
-    0x00000000, 0x00000000, 0x00000000, 0x00000000,
-    0x00000001, 0x00000000, 0x00000065, 0x00000002,
-    0x00000005, 0x00000004, 0xffffffff, 0x00000000,
-    0x00000001, 0x0000000c, 0x706d6173, 0x3072656c,
-    0x78657400, 0x65727574, 0x694d0030, 0x736f7263,
-    0x2074666f, 0x20295228, 0x4c534c48, 0x61685320,
-    0x20726564, 0x706d6f43, 0x72656c69, 0x2e303120,
-    0xabab0031, 0x4e475349, 0x0000006c, 0x00000003,
-    0x00000008, 0x00000050, 0x00000000, 0x00000001,
-    0x00000003, 0x00000000, 0x0000000f, 0x0000005c,
-    0x00000000, 0x00000000, 0x00000003, 0x00000001,
-    0x00000f0f, 0x00000062, 0x00000000, 0x00000000,
-    0x00000003, 0x00000002, 0x00000303, 0x505f5653,
-    0x5449534f, 0x004e4f49, 0x4f4c4f43, 0x45540052,
-    0x4f4f4358, 0xab004452, 0x4e47534f, 0x0000002c,
-    0x00000001, 0x00000008, 0x00000020, 0x00000000,
-    0x00000000, 0x00000003, 0x00000000, 0x0000000f,
-    0x545f5653, 0x65677261, 0xabab0074, 0x52444853,
-    0x00000094, 0x00000040, 0x00000025, 0x0300005a,
-    0x00106000, 0x00000000, 0x04001858, 0x00107000,
-    0x00000000, 0x00005555, 0x03001062, 0x001010f2,
-    0x00000001, 0x03001062, 0x00101032, 0x00000002,
-    0x03000065, 0x001020f2, 0x00000000, 0x02000068,
-    0x00000001, 0x09000045, 0x001000f2, 0x00000000,
-    0x00101046, 0x00000002, 0x00107e46, 0x00000000,
-    0x00106000, 0x00000000, 0x07000038, 0x001020f2,
-    0x00000000, 0x00100e46, 0x00000000, 0x00101e46,
-    0x00000001, 0x0100003e, 0x54415453, 0x00000074,
-    0x00000003, 0x00000001, 0x00000000, 0x00000003,
-    0x00000001, 0x00000000, 0x00000000, 0x00000001,
-    0x00000000, 0x00000000, 0x00000000, 0x00000000,
-    0x00000000, 0x00000000, 0x00000001, 0x00000000,
-    0x00000000, 0x00000000, 0x00000000, 0x00000000,
-    0x00000000, 0x00000000, 0x00000000, 0x00000000,
-    0x00000000, 0x00000000, 0x00000000, 0x00000000,
-    0x00000000
+    _DXBC, 0xa5e778be, 0xeb700c19, 0x1facb14c, 0x97e98416,
+    0x00000001, 0x00000170, 3,
+    0x0000002c, 0x000000a0, 0x000000d4,
+    _ISGN, 0x0000006c, 3, 0x00000008,
+    0x00000050, 0, 1, 3, 0, 0x000f,
+    0x0000005c, 0, 0, 3, 1, 0x0f0f,
+    0x00000062, 0, 0, 3, 2, 0x0303,
+    0x505f5653, 0x5449534f, 0x004e4f49, 0x4f4c4f43, 0x45540052, 0x4f4f4358, 0xab004452,
+    _OSGN, 0x0000002c, 1, 0x00000008,
+    0x00000020, 0, 0, 3, 0, 0x000f,
+    0x545f5653, 0x45475241, 0xabab0054,
+    _SHDR, 0x00000094, _VER(PIXEL_SHADER, 4, 0), 0x00000025,
+    _OP(DCL_SAMPLER, 3), _0(1D, SAMPLER), D3D10_SB_SAMPLER_MODE_DEFAULT,                                        // dcl_sampler s0, mode_default
+    _OPDIM(DCL_RESOURCE, 4, TEXTURE2D), _0(1D, RESOURCE), 0, D3D10_SB_RETURN_TYPE_FLOAT4,                       // dcl_resource_texture2d(float,float,float,float) t0
+    _OPINT(DCL_INPUT_PS, 3, LINEAR), _4M(1D, INPUT), 1,                                                         // dcl_input_ps linear v1.xyzw
+    _OPINT(DCL_INPUT_PS, 3, LINEAR), _4MM(1D, INPUT, XY), 2,                                                    // dcl_input_ps linear v2.xy
+    _OP(DCL_OUTPUT, 3), _4M(1D, OUTPUT), 0,                                                                     // dcl_output o0.xyzw
+    _OP(DCL_TEMPS, 2), 1,                                                                                       // dcl_temps 1
+    _OP(SAMPLE, 9), _4M(1D, TEMP), 0, _4SS(1D, INPUT, XYXX), 2, _4S(1D, RESOURCE), 0, _0(1D, SAMPLER), 0,       // sample r0.xyzw, v2.xyxx, t0.xyzw, s0
+    _OP(MUL, 7), _4M(1D, OUTPUT), 0, _4S(1D, TEMP), 0, _4S(1D, INPUT), 1,                                       // mul o0.xyzw, r0.xyzw, v1.xyzw
+    _OP(RET, 1)                                                                                                 // ret
 };
 //------------------------------------------------------------------------------
--- graphic/xxGraphicD3DAsm.h
+++ graphic/xxGraphicD3DAsm.h
@@ -3,11 +3,13 @@
 #include "xxSystem.h"
 
 //==============================================================================
-//  Direct3D Assembly Shader
+//  Direct3D 8.0 Assembly Shader
 //==============================================================================
 xxEXTERN const DWORD vertexShaderCode10[];
 xxEXTERN const DWORD vertexShaderCode11[];
 xxEXTERN const DWORD pixelShaderCode11[];
-//------------------------------------------------------------------------------
+//==============================================================================
+//  Direct3D 10.0 Assembly Shader
+//==============================================================================
 xxEXTERN const DWORD vertexShaderCode40[];
 xxEXTERN const DWORD pixelShaderCode40[];
